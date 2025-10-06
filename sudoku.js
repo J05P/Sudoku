7d311a66679815b503a11461c4fd9accaf9433bf
J05P <jppereiramoreno@gmail.com>
2025-10-06 19:19:22 +0200

    poner numeros donde quiera

--- sudoku.js
+++ sudoku.js
@@ -3,12 +3,12 @@ var guardar="";
 var tableroPrincipal=document.getElementById("tablero")
 var tabla=document.createElement("table")
 for (let i = 0; i < 9; i++) {
-    var fila = tabla.insertRow()
+    let fila = tabla.insertRow()
     for (let j = 0; j <9; j++) {
         var celda = fila.insertCell() 
         celda.onclick=function(){
-            fila.innerText=guardar;
-        }
+            celda.innerText=guardar;
+        } 
     }
 }
 tableroPrincipal.appendChild(tabla)
@@ -20,17 +20,13 @@ var Numeros = document.getElementById("numeros");
 var TNumeros = document.createElement("table");
 var filaNumeros = TNumeros.insertRow();
 for (let numero = 1; numero <= 10; numero++) {
-    var celda = filaNumeros.insertCell();
-    celda.id=numero;
-    celda.addEventListener("click",function(){
-        if(numero ==10){numero="X"}
-        guardar=numero;
-    })
-    if(numero ==10){
-        celda.textContent="X";
-    }else{
-        celda.textContent = numero;
-    }
+    let valor = numero === 10 ? "X" : numero;
+    let celda2 = filaNumeros.insertCell();
+    celda2.id = valor;
+    celda2.textContent = valor;
+    celda2.addEventListener("click", function () {
+        guardar = valor;
+    });
 }
 Numeros.appendChild(TNumeros);
 //
